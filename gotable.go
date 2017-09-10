1cce94737d10e5aa338f11aeb0d6239bcd284082
Malcolm Gorman <malcolm.gorman@gmail.com>
2017-09-10 17:58:45 +1000

    Slight change to package documentation for testing godoc.

--- gotable.go
+++ gotable.go
@@ -1,6 +1,6 @@
 // Copyright (c) 2017 Malcolm Gorman
 
-// A tabular data format for configurations and messaging.
+// A tabular data format for configurations and messaging, with helper functions.
 package gotable
 
 import (
