3c8519808682e3f11358e58d9b0707965763d8d9
Malcolm Gorman <malcolm.gorman@gmail.com>
2018-12-13 07:59:54 +1100

    Move shadow.sh to ~/bin/shadow

--- shadow.sh
+++
@@ -1,6 +0,0 @@
-# Note: this flag may be one that is by default off even with all turned on.
-# See: https://golang.org/cmd/vet
-#      Shadowed variables
-#      Flag: -shadow=false (experimental; must be set explicitly)
-
-go vet -shadow=true
