3a2146ea243c3053e5a313f84a3fbd3d1fe9f672
Malcolm Gorman <malcolm.gorman@gmail.com>
2019-07-13 20:10:42 +1000

    Move a test to flatbuffers

--- gotables_test.go
+++ gotables_test.go
@@ -3,7 +3,6 @@ package gotables
 import (
 	"bytes"
 	"fmt"
-	"go/token"
 	"io/ioutil"
 	"log"
 	"math"
@@ -8713,50 +8712,4 @@ func isGoKeyword(name string) bool {
     nameLower := strings.ToLower(name)
     _, exists := goKeywords[nameLower]
     return exists
-}
-
-func TestIsGoKeyword(t *testing.T) {
-	var tests = []struct {
-		maybeKeyword string
-		isKeyword bool
-	}{
-		{"break", true},
-		{"case", true},
-		{"chan", true},
-		{"const", true},
-		{"continue", true},
-		{"default", true},
-		{"defer", true},
-		{"else", true},
-		{"fallthrough", true},
-		{"for", true},
-		{"func", true},
-		{"go", true},
-		{"goto", true},
-		{"if", true},
-		{"import", true},
-		{"interface", true},
-		{"map", true},
-		{"package", true},
-		{"range", true},
-		{"return", true},
-		{"select", true},
-		{"struct", true},
-		{"switch", true},
-		{"type", true},
-		{"var", true},
-		{"int", false},
-	}
-
-	for i, test := range tests {
-		var result bool = token.Lookup(test.maybeKeyword).IsKeyword()
-
-		if result != test.isKeyword && test.isKeyword == true {
-			t.Errorf("test[%d] expected %s to be a Go keyword, but it's not", i, test.maybeKeyword)
-		}
-
-		if result != test.isKeyword && test.isKeyword == false {
-			t.Errorf("test[%d] expected %s to NOT be a Go keyword, but it is", i, test.maybeKeyword)
-		}
-	}
 }
